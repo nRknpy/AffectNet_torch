5afe55dde8a380c4b87750f79fa918b07c5bb14f
nRknpy <ofuro.jabu.jabu@gmail.com>
2023-06-17 01:14:05 +0900

    duplicate plot

--- torchaffectnet/utils.py
+++ torchaffectnet/utils.py
@@ -1,10 +1,10 @@
 from PIL import Image
 from tqdm import tqdm
 import torch
-from umap.umap_ import UMAP
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import matplotlib.cm as cm
+# from umap.umap_ import UMAP
+# import matplotlib.pyplot as plt
+# import matplotlib.colors as colors
+# import matplotlib.cm as cm
 
 from .const import ID2LABEL
 
@@ -15,41 +15,41 @@ def pil_loader(path):
         return img.convert('RGB')
 
 
-def cls_tokens(model, tokenizer, dataset, device):
-    tokens = []
-    labels = []
-    for img, label in tqdm(dataset):
-        # feature = tokenizer(img, return_tensors='pt').pixel_values.to(device)
-        with torch.no_grad():
-            token = model(img.unsqueeze(0).to(device),
-                          output_hidden_states=True).hidden_states[-1][0, 0, :]
-        tokens.append(token.cpu())
-        labels.append(label)
-    return torch.stack(tokens).squeeze(), torch.tensor(labels)
+# def cls_tokens(model, tokenizer, dataset, device):
+#     tokens = []
+#     labels = []
+#     for img, label in tqdm(dataset):
+#         # feature = tokenizer(img, return_tensors='pt').pixel_values.to(device)
+#         with torch.no_grad():
+#             token = model(img.unsqueeze(0).to(device),
+#                           output_hidden_states=True).hidden_states[-1][0, 0, :]
+#         tokens.append(token.cpu())
+#         labels.append(label)
+#     return torch.stack(tokens).squeeze(), torch.tensor(labels)
 
 
-def plot_tokens(tokens, labels, n_neighbors, id2label=ID2LABEL):
-    umap = UMAP(n_neighbors=n_neighbors)
-    zs = umap.fit_transform(tokens.numpy())
-    ys = labels.numpy()
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.set_xlabel('feature-1')
-    ax.set_ylabel('feature-2')
-    cmap = cm.get_cmap('gist_ncar')
+# def plot_tokens(tokens, labels, n_neighbors, id2label=ID2LABEL):
+#     umap = UMAP(n_neighbors=n_neighbors)
+#     zs = umap.fit_transform(tokens.numpy())
+#     ys = labels.numpy()
+#     fig = plt.figure()
+#     ax = fig.add_subplot()
+#     ax.set_xlabel('feature-1')
+#     ax.set_ylabel('feature-2')
+#     cmap = cm.get_cmap('gist_ncar')
 
-    label2point = {}
-    for x, y in zip(zs, ys):
-        mp = ax.scatter(x[0], x[1],
-                        alpha=1,
-                        label=id2label[y],
-                        # c=label2color[y],
-                        c=y,
-                        cmap=cmap,
-                        vmin=0,
-                        vmax=len(set(ys)),
-                        s=3,)
-        label2point[id2label[y]] = mp
-    labels, handles = zip(*sorted(label2point.items()))
-    fig.legend(handles, labels, loc='right')
-    plt.show()
+#     label2point = {}
+#     for x, y in zip(zs, ys):
+#         mp = ax.scatter(x[0], x[1],
+#                         alpha=1,
+#                         label=id2label[y],
+#                         # c=label2color[y],
+#                         c=y,
+#                         cmap=cmap,
+#                         vmin=0,
+#                         vmax=len(set(ys)),
+#                         s=3,)
+#         label2point[id2label[y]] = mp
+#     labels, handles = zip(*sorted(label2point.items()))
+#     fig.legend(handles, labels, loc='right')
+#     plt.show()
